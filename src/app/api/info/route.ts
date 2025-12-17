e809bf69c0af4ac4dbfb997f9cf7000032c9356b
petarcorluka86 <petarcorluka86@gmail.com>
2025-12-17 12:28:11 +0100

    rm-log

--- src/app/api/info/route.ts
+++ src/app/api/info/route.ts
@@ -37,7 +37,6 @@ export async function GET(req: NextRequest) {
         Accept: "*/*",
       },
     });
-    console.log("response", response);
 
     if (!response.ok) {
       return NextResponse.json(
